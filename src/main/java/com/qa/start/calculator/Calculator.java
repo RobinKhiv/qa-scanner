d9ec07ab2d2965e136516f00801c2e0b0d234269
Robin Khiv <robinkhiv@gmail.com>
2021-05-25 12:35:47 -0700

    rk:added calculator class

--- src/main/java/com/qa/start/calculator/Calculator.java
+++ src/main/java/com/qa/start/calculator/Calculator.java
@@ -1,5 +1,25 @@
 package com.qa.start.calculator;
 
 public class Calculator {
-
+	public static int add(int x, int y) {
+		return x+y;
+	}
+	public static double add(double x, double y) {
+		return x+y;
+	}
+	public static int subtract(int x, int y) {
+		return x-y;
+	}
+	public static double subtract(double x, double y) {
+		return x-y;
+	}
+	public static int multiply(int x, int y) {
+		return x*y;
+	}
+	public static double multiply(double x, double y) {
+		return x*y;
+	}
+	public static double divide(double x, double y) {
+		return x/y;
+	}
 }
